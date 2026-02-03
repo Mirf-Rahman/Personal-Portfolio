23494912000d222584058f0c259662254108312d
Mir Faiyazur Rahman <mirfaiyazrahman@gmail.com>
2026-02-03 00:12:17 -0500

    lamp comp

--- frontend/app/src/components/ui/lamp.tsx
+++ frontend/app/src/components/ui/lamp.tsx
@@ -14,7 +14,7 @@ export const LampContainer = ({
     <div
       className={cn(
         "relative flex flex-col items-center justify-start overflow-visible bg-transparent w-full z-0",
-        className
+        className,
       )}
     >
       <div className="relative flex w-full flex-1 items-start justify-center isolate z-0">
@@ -30,8 +30,7 @@ export const LampContainer = ({
             backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
           }}
           className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top] [mask-image:linear-gradient(to_bottom,white,transparent)]"
-        >
-        </motion.div>
+        ></motion.div>
         <motion.div
           initial={{ opacity: 0.3, width: "15rem" }}
           whileInView={{ opacity: 0.8, width: "30rem" }}
@@ -44,8 +43,7 @@ export const LampContainer = ({
             backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
           }}
           className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-cyan-500 text-white [--conic-position:from_290deg_at_center_top] [mask-image:linear-gradient(to_bottom,white,transparent)]"
-        >
-        </motion.div>
+        ></motion.div>
         <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-transparent blur-2xl"></div>
         <div className="absolute inset-auto z-50 h-36 w-[28rem] translate-y-12 rounded-full bg-cyan-500 opacity-30 blur-3xl"></div>
         <motion.div
